f15fcdfc9c518845bd6dacd3c5f090dd830b90b3
Angel Salazar <angelsalazar1992@hotmail.com>
2020-09-02 20:34:19 -0600

    formmatting files

--- envImpl.py
+++ envImpl.py
@@ -12,105 +12,102 @@ TURN = 'Turn'
 ADVANCED = 'Advance'
 STAY = 'Stay'
 
-class FixAgentLocation:
-  def __init__(self, x = 0, y = 0, d = None):
-    self.x = x
-    self.y = y
-    self.d = d
-
 class Grid(Environment):
-  printGrid = []
-  
-  def __init__(self, envType = 'FULLY', initialAgentLocation = None):
-    super(Grid, self).__init__()
-    for i in range(25):
-      self.printGrid.append({"A":0,"G":0,"T":0})
-    self.envType = envType
-    self.MAX_WIDTH = self.MAX_HEIGHT = 5
-    self.initialAgentLocation = initialAgentLocation
+    printGrid = []
 
-  def thing_classes(self):
-    return [SimpleReflexAgent, Trap, Gold]  # List of classes that can go into environment
+    def __init__(self, envType = 'FULLY'):
+        super(Grid, self).__init__()
+        for i in range(25):
+          self.printGrid.append({"A":0,"G":0,"T":0})
+        self.envType = envType
+        self.MAX_WIDTH = self.MAX_HEIGHT = 5
 
-  def __str__(self):
-    r = ""
-    for i in self.printGrid:
-      r += str(i)+"\n"
+    def thing_classes(self):
+        return [SimpleReflexAgent, Trap, Gold]  # List of classes that can go into environment
 
-    return r
+    def __str__(self):
+        r = ""
+        for i in self.printGrid:
+            r += str(i)+"\n"
+        return r
 
-  def percept(self, agent):
-    things = self.things;
 
-    if self.envType == 'PARTIALLY':
-      movements = [(1,0),(1,1),(0,1),(-1,-1),(-1,0),(0,-1),(-1,1),(1,-1)]
-      percepts = []
-      for m in movements:
-        newX = agent.location[0] + m[0]
-        newY = agent.location[1] + m[1]
-        if newX < 5 and newX >= 0 and newY < 5 and newY >= 0:
-          percepts.append((newX,newY))
+    def percept(self, agent):
+        things = self.things;
 
-        for thing in things:
-          if ((thing.location[0],thing.location[1]) not in percepts):
-            things.remove(thing)
+        if self.envType == 'FULLY':
+            pass
+        
+        # Only retrieves things visible
+        if self.envType == 'PARTIALLY':
+            movements = [(1,0),(1,1),(0,1),(-1,-1),(-1,0),(0,-1),(-1,1),(1,-1)]
+            percepts = []
+            for m in movements:
+                newX = agent.location[0] + m[0]
+                newY = agent.location[1] + m[1]
+            if newX < 5 and newX >= 0 and newY < 5 and newY >= 0:
+                percepts.append((newX,newY))
 
-    print(things)
-    return things
+            for thing in things:
+                if ((thing.location[0],thing.location[1]) not in percepts):
+                    things.remove(thing)
 
-  def add_thing(self, thing, location = None):
-    # set random location if not provided
-    thing.location = location if location is not None else self.default_location(thing)
+        print(things)
+        return things
 
-    print(thing.location)
-    # if thing is instance of Agent
-    if (isinstance(thing, Agent)):
-      self.printGrid[thing.location[0]*5 + thing.location[1]]["A"] += 1
-      # initialize thing's perform
-      thing.performance = 100
-      # register agent
-      self.agents.append(thing)
-    else:
-      if (isinstance(thing,Gold)):
-        self.printGrid[thing.location[0]*5 + thing.location[1]]["G"] += 1
-      else:
-        self.printGrid[thing.location[0]*5 + thing.location[1]]["T"] += 1
-      # register thing 2
-      self.things.append(thing)
+    def add_thing(self, thing, location = None):
+        # set random location if not provided
+        thing.location = location if location is not None else self.default_location(thing)
 
-  def execute_action(self, agent, action):
-    if action == TURN:
-      agent.turn()
-    elif action == ADVANCED:
-      agent.forward()
-      self.consumeThingsAtAgentLocation(agent)
-    elif action == STAY:
-      self.consumeThingsAtAgentLocation(agent)
+        print(thing.location)
+        # if thing is instance of Agent
+        if (isinstance(thing, Agent)):
+            self.printGrid[thing.location[0]*5 + thing.location[1]]["A"] += 1
+            # initialize thing's perform
+            thing.performance = 100
+            # register agent
+            self.agents.append(thing)
+        else:
+            if (isinstance(thing,Gold)):
+                self.printGrid[thing.location[0]*5 + thing.location[1]]["G"] += 1
+            else:
+                self.printGrid[thing.location[0]*5 + thing.location[1]]["T"] += 1
+            # register thing 2
+            self.things.append(thing)
 
-  # generatas a random location for the given thing
-  def default_location(self, thing):
-    # generate random x, y, and default direction
-    x = random.randint(0, self.MAX_WIDTH - 1);
-    y = random.randint(0, self.MAX_HEIGHT - 1);
-    d = Direction(Direction.R)
+    def execute_action(self, agent, action):
+        if action == TURN:
+            agent.turn()
+        elif action == ADVANCED:
+            agent.forward()
+            self.consumeThingsAtAgentLocation(agent)
+        elif action == STAY:
+            self.consumeThingsAtAgentLocation(agent)
 
-    # if initialAgentLocation is given and given thing is instance of agent
-    if (self.initialAgentLocation and isinstance(thing, Agent)):
-      # initialize agent at specific location
-      x = self.initialAgentLocation.x
-      y = self.initialAgentLocation.y
-      d = self.initialAgentLocation.d
+    # generatas a random location for the given thing
+    def default_location(self, thing):
+        # generate random x, y, and default direction
+        x = random.randint(0, self.MAX_WIDTH - 1);
+        y = random.randint(0, self.MAX_HEIGHT - 1);
+        d = Direction(Direction.R)
 
-    # return thing direction
-    return (x, y, d)
+        # if initialAgentLocation is given and given thing is instance of agent
+        if (self.initialAgentLocation and isinstance(thing, Agent)):
+            # initialize agent at specific location
+            x = self.initialAgentLocation.x
+            y = self.initialAgentLocation.y
+            d = self.initialAgentLocation.d
 
-  def consumeThingsAtAgentLocation(self, agent):
-    traps = self.list_things_at(agent.location, Trap)
-    if len(traps) > 0:
-      self.delete_thing(traps[0])
-      agent.modifyPerformance(-5)
+        # return thing direction
+        return (x, y, d)
 
-    ores = self.list_things_at(agent.location, Gold)
-    if len(ores) > 0:
-      self.delete_thing(ores[0])
-      agent.modifyPerformance(10)
+    def consumeThingsAtAgentLocation(self, agent):
+        traps = self.list_things_at(agent.location, Trap)
+        if len(traps) > 0:
+            self.delete_thing(traps[0])
+            agent.modifyPerformance(-5)
+
+        ores = self.list_things_at(agent.location, Gold)
+        if len(ores) > 0:
+            self.delete_thing(ores[0])
+            agent.modifyPerformance(10)
--- simpleReflexAgent.py
+++ simpleReflexAgent.py
@@ -1,41 +1,65 @@
 from agents import Agent
 from agents import Direction
 
+from envImpl import TURN
+from envImpl import ADVANCED
+from envImpl import STAY
+
+# act only based on the current perception
+# ignore the rest of percept history
+# based on if-then rules
+# environment should be fully observable
+
+def simpleReflexAgentProgram(percept):
+    status
+    if ()
+    pass
+    # location, status = percept
+    # if status == 'Dirty':
+    #     return 'Suck'
+    # elif location == loc_A:
+    #     return 'Right'
+    # elif location == loc_B:
+    #     return 'Left'
+
 class SimpleReflexAgent(Agent):
+    def __init__(self, envType = 'FULLY', initialAgentLocation = None):
+      super(SimpleReflexAgent, self).__init__(program=simpleReflexAgentProgram)
 
-  def modifyPerformance(self, amount):
-    self.performance += amount
+    def modifyPerformance(self, amount):
+        self.performance += amount
 
-  def checkBounds():
-    newX, newY = None
-    if (currentDir.direction == Direction.U):
-      newX = self.location[0] - 1
-    elif (currentDir.direction == Direction.R):
-      newY = self.location[1] + 1
-    elif (currentDir.direction == Direction.B):
-      newX = self.location[0] + 1
-    elif (currentDir.direction == Direction.L):
-      newY = self.location[1] - 1
+    def checkBounds():
+        newX, newY = None
+        if (currentDir.direction == Direction.U):
+          newX = self.location[0] - 1
+        elif (currentDir.direction == Direction.R):
+          newY = self.location[1] + 1
+        elif (currentDir.direction == Direction.B):
+          newX = self.location[0] + 1
+        elif (currentDir.direction == Direction.L):
+          newY = self.location[1] - 1
 
-    if newX < 0 or newY < 0 or newX >= 5 or newY >= 5:
-      return None
-    else:
-      return (newX,newY,self.location[2])
-  def turn(self):
-    self.modifyPerformance(-1)
-    currentDir = self.location[2];
-    # d + Direction.R
-    if (currentDir.direction == Direction.U):
-      currentDir = currentDir + Direction.R
-    elif (currentDir.direction == Direction.R):
-      currentDir = currentDir + Direction.B
-    elif (currentDir.direction == Direction.B):
-      currentDir = currentDir + Direction.L;
-    elif (currentDir.direction == Direction.L):
-      currentDir = currentDir + Direction.U;
+        if newX < 0 or newY < 0 or newX >= 5 or newY >= 5:
+          return None
+        else:
+          return (newX,newY,self.location[2])
 
-  def forward(self):
-    self.modifyPerformance(-1)
-    newLocation = self.checkBounds()
-    if newLocation:
-      self.location = newLocation
+    def turn(self):
+        self.modifyPerformance(-1)
+        currentDir = self.location[2];
+        # d + Direction.R
+        if (currentDir.direction == Direction.U):
+          currentDir = currentDir + Direction.R
+        elif (currentDir.direction == Direction.R):
+          currentDir = currentDir + Direction.B
+        elif (currentDir.direction == Direction.B):
+          currentDir = currentDir + Direction.L;
+        elif (currentDir.direction == Direction.L):
+          currentDir = currentDir + Direction.U;
+
+    def forward(self):
+        self.modifyPerformance(-1)
+        newLocation = self.checkBounds()
+        if newLocation:
+          self.location = newLocation
